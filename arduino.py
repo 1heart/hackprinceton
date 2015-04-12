33faa089fa5e2ba90e1c55d7e4b7e91212b51b01
Fernando Trujano <trujano@mit.edu>
2015-04-11 21:13:13 -0400

    Basic arduino animation

--- arduino.py
+++ arduino.py
@@ -1,9 +1,11 @@
 # Python arduino interface
-import serial
+import serial, time
 ser = serial.Serial('/dev/tty.usbmodem1411', 9600)
 # while True:
 #     print ser.readline();
 
+ser.write("0.57")
 
-ser.write("1.45")
+
+
 print "done"
